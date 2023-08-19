13b927ba40e8f48074c91ff98ef4000e99460737
Davi Caamano <davi.caamano@shiftpixy.com>
2023-08-19 19:39:49 -0400

    chore: trimming untilized packages

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,13 @@
+/* istanbul ignore file */
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 async function bootstrap() {
   const logger = new Logger('EntryPoint');
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Leaves Tracker')
-    .setDescription('Api Docs for leaves tracker')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
   const PORT = 5002;
 
   await app.listen(PORT);
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -4,6 +4,7 @@ import store from "../src/store";
 import "../src/styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />;
