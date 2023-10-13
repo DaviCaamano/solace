cfff0ea7a8a9b542bef7153a92b6d9b798a7bc81
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-13 05:32:01 -0400

    fix: fix for user menu

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,9 +3,9 @@ import { Content } from '@components/landing';
 
 export default function Web() {
   return (
-    <div id={'home-page'} className={'flex flex-col min-w-screen min-h-screen relative'}>
-      <Header />
+    <div id={'home-page'} className={'flex flex-col-reverse min-w-screen min-h-screen relative'}>
       <Content />
+      <Header />
     </div>
   );
 }
