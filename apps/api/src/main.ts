1459884dc447cc0afba77e4def7d7d86118fc881
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-21 06:55:34 -0400

    feat: synchronized env vars between environments.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,19 +5,19 @@ require('dotenv').config({ path: '../../.env' });
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '~app.module';
-const PORT = process.env.PORT || 8000;
+const port = process.env.NEXT_PUBLIC_BACKEND_PORT || 8000;
 declare const module: any;
 async function bootstrap() {
   const logger = new Logger('EntryPoint');
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
 
-  await app.listen(PORT);
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-  logger.log(`Server running on http://localhost:${PORT}`);
+  logger.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,30 +1,33 @@
 require('dotenv').config({ path: '../../.env' });
 
 const jsonImporter = require('node-sass-json-importer');
-const backendPort = process.env.PORT || 8000;
-const backendHost = process.env.BACKEND_HOST || 'http://localhost';
-
+const backendPort = process.env.NEXT_PUBLIC_BACKEND_PORT || 8000;
+let backendHost = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost';
+//Next js redirection does not allow localhost.
+if (backendHost === 'http://localhost') {
+  backendHost = 'http://127.0.0.1';
+}
 module.exports = {
-    reactStrictMode: true,
-    transpilePackages: ["shared"],
-    async rewrites() {
-        return {
-            fallback: [
-                {
-                    source: '/api/:path*',
-                    destination: `${backendHost}:${backendPort}/api/:path*`, // Proxy to Backend
-                },
-            ],
-        };
-    },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            use: ['@svgr/webpack'],
-        });
-        return config;
-    },
-    sassOptions: {
-        importer: jsonImporter(),
-    },
+  reactStrictMode: true,
+  transpilePackages: ['shared'],
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/api/:path*',
+          destination: `${backendHost}:${backendPort}/api/:path*`, // Proxy to Backend
+        },
+      ],
+    };
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  sassOptions: {
+    importer: jsonImporter(),
+  },
 };
